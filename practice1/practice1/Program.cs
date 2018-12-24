a843597be1bc1b27540f072d88f7ee64686f93b0
carrie222 <19910517882@163.com>
2018-12-24 16:14:40 +0800

    使用vs实现接口

---
+++ practice1/practice1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+interface IMyInterface
+{
+    // 接口成员
+    void MethodToImplement();
+}
+
+class InterfaceImplementer : IMyInterface
+{
+    static void Main()
+    {
+        InterfaceImplementer iImp = new InterfaceImplementer();
+        iImp.MethodToImplement();
+    }
+
+    public void MethodToImplement()
+    {
+        Console.WriteLine("MethodToImplement() called.");
+    }
+}
